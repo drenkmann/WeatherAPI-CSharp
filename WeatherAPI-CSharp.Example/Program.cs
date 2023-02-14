6783ea176a8612643bca601a151ac1babb206c9c
Skratymir <Skratymir@users.noreply.github.com>
2023-02-14 21:54:15 +0000

    deploy: 23976ca6594b3b2685673a4d899f72f3d039890a

--- WeatherAPI-CSharp.Example/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using WeatherAPI_CSharp;
-
-var client = new APIClient("YOUR-API-KEY", true);
-
-var weather = await client.GetWeatherCurrentAsync("Berlin");
-Console.WriteLine($"Die Temperatur in Berlin betraegt {weather.TemperatureCelsius}C bei einer Windgeschwindingkeit von {weather.WindKph}km/h");
--- docs/index.md
+++
@@ -1,2 +0,0 @@
-# WeatherAPI CSharp
-This is a small library to be used with the weatherapi.com API
